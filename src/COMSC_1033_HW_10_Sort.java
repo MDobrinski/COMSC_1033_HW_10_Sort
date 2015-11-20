1ced109840a91bfa61a9dc66db29aeda4160fdb9
Michael Dobrinski <michael.git2015@gmail.com>
2015-11-19 20:14:12 -0600

    Modified sort program from homework 4.

--- src/COMSC_1033_HW_10_Sort.java
+++ src/COMSC_1033_HW_10_Sort.java
@@ -1,64 +1,45 @@
 /**
  * Author Michael Dobrinski COMSC 1033 Section 1411 Instructor: Dr. Evert
- * 19 September 2015
+ * 19 November 2015
  */
 /**
- * Homework number 4 Create a program to read in 3 numbers then sort
+ * Homework number 10 Create a program to take 6 numbers then sort
  * them into ascending order.
  *
  * Solution:
- * The program prompts the user to enter the number of integers to be sorted
- * then prompts for each number in turn. The numbers are stored in an array
- * as they are entered and then the array is printed in the order that the
- * numbers were entered. After the numbers are printed in the original order
+ * The program generates 6 random integers between 0 and 2000. The numbers are
+ * stored in an array and then the array is printed in the order that the
+ * numbers were generated. After the numbers are printed in the original order
  * the array is sent to the method insertSort(int[] sortList, int c) where the
  * array is sorted using an insertion sort. Once the array is sorted it is
  * printed out with the numbers in ascending order.
  *
- * The method outputArray(int[] sortList, int x) is used to print the array.
+ * The method outputArray(int[] sortList, int x) is used to print the array and
+ * the method MyRandoms.randomNumber(int min, int max) is used to generate the
+ * random numbers.
  */
-
-import java.util.Scanner;
-import java.util.InputMismatchException;
 
 public class COMSC_1033_HW_10_Sort {
 
 	public static void main(String[] args) {
-		int[] sortList = new int[21];
-		int i = 0, count=0;
-		boolean continueProgram = false;
+		int i = 0, count = 6;
+		int[] sortList = new int[count];
 
-			System.out.print("\nThe program reads in a list of up to 20 integer"+
+			System.out.print("\nThe program generates a list of 6 integer"+
 				" numbers then sorts the numbers and prints them in ascending"+
 				" order.\n");
-			Scanner input = new Scanner(System.in);
-				do{
-					try{
-						System.out.print("\nEnter the number of integers to be sorted, "
-								+ "the maximum is 20: ");
-						count = input.nextInt();
-						System.out.printf("\nEnter %d numbers separated by a space: ",
-								+count);
-						count = (count > 20) ? 20 : count; // Limit the list to 20 max.
-						for (i = 0; i < count; i++) {      // loop to get the numbers
-							sortList[i] = input.nextInt();
-						}
-						continueProgram = false;//Set to exit DO-WHILE loop & end program.
-					}   // End of try block
-					catch (InputMismatchException ex){
-						System.out.print("\nERROR: Invalid input type. The numbers must" +
-								" be integers.");
-						input.nextLine();
-						continueProgram = true;
-					}   // End of catch block
-				} while (continueProgram);
-				System.out.println("\nThe numbers in the original order are:");
-				outputArray(sortList, count);   // print the original array
-				insertSort(sortList, count);
-				System.out.println("\nThe numbers in ascending order are:");
-				outputArray(sortList, count);   // print the sorted array
 
-				input.close();
+			for (i = 0; i < count; i++) {      // loop to get the numbers
+				sortList[i] = MyRandoms.randomNumber(0, 2000);
+			}
+
+			System.out.println("\nThe numbers in the original order are:");
+			outputArray(sortList, count);   // print the original array
+			insertSort(sortList, count);
+			System.out.println("\nThe numbers in ascending order are:");
+			outputArray(sortList, count);   // print the sorted array
+
+
 	} // ******************** END of Main *************************
 
 	/**
@@ -80,7 +61,7 @@ public class COMSC_1033_HW_10_Sort {
 	public static void insertSort(int[] sortList, int c) {
 		int i = 0, j = 0, temp = 0;
 
-		for (j = 1; j <= c; j++) {   // pick each number starting at the
+		for (j = 1; j <= c-1; j++) {   // pick each number starting at the
 			temp = sortList[j];      // second element
 			i = j - 1;
 																		 // Then look for a place to insert it
@@ -92,4 +73,4 @@ public class COMSC_1033_HW_10_Sort {
 		}
 	} // END of insertSort
 
-} // END of COMSC_1033_HW_4_SORT class
+} // END of COMSC_1033_HW_10_SORT class
